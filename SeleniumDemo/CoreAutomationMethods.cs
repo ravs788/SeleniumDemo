fb7c602ad5a5b17ab9a93eaaf533080b76807754
Ravi Shankar <ravshan@microsoft.com>
2020-01-26 06:43:26 +0530

    Updated solution for a new flow

--- SeleniumDemo/CoreAutomationMethods.cs
+++ SeleniumDemo/CoreAutomationMethods.cs
@@ -28,7 +28,7 @@ namespace SeleniumDemo
 
             var ChromeDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             driver = new ChromeDriver(ChromeDriverPath);//, chromeOptions);
-            driver.Url = "https://s1.demo.opensourcecms.com/wordpress/";
+            driver.Url = "https://s1.demo.opensourcecms.com/wordpress/wp-login.php";
 
             driver.Manage().Window.Maximize();            
         }
@@ -39,32 +39,60 @@ namespace SeleniumDemo
 
             var userName = "opensourcecms";
 
-            IWebElement lnkLogin = driver.FindElement(By.XPath("//a[text()='Log in']"));
-            lnkLogin.Click();
-
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
-            wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@id='user_login']")));
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//input[@id='user_login']")));
 
             IWebElement txtLogin = driver.FindElement(By.XPath("//input[@id='user_login']"));
             txtLogin.SendKeys(userName);
-            
+
             IWebElement txtPasswd = driver.FindElement(By.XPath("//input[@id='user_pass']"));
             txtPasswd.SendKeys(userName);
 
             IWebElement btnLogin = driver.FindElement(By.XPath("//input[@value='Log In']"));
             btnLogin.Click();
 
-            wait.Until(ExpectedConditions.ElementExists(By.XPath("//ul[@class='ab-top-secondary ab-top-menu']/li//span[text()='"+userName+"']")));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//ul[@class='ab-top-secondary ab-top-menu']/li//span[text()='"+userName+"']")));
 
             Actions action = new Actions(driver);
-            action.MoveToElement(driver.FindElement(By.XPath("//ul[@class='ab-top-secondary ab-top-menu']/li/a"))).Perform();
+            action.MoveToElement(driver.FindElement(By.XPath("//div[@id='adminmenuwrap']/ul/li[@id='menu-posts']//div[text()='Posts']"))).Perform();
 
-            
+            IWebElement lnkAddNew = driver.FindElement(By.XPath("//div[@id='adminmenuwrap']/ul/li[@id='menu-posts']/ul[contains(@class,'wp-submenu-wrap')]/li/a[text()='Add New']"));
+            lnkAddNew.Click();
+
+            IList<IWebElement> lnkPosts = driver.FindElements(By.XPath("//div[@id='adminmenuwrap']/ul/li[@id='menu-posts']//div[text()='Posts']"));
+            Assert.IsTrue(lnkPosts.Count == 1, "Add New Post opened.");
+
+            IList<IWebElement> pnlTip = driver.FindElements(By.XPath("//div[@class='components-popover__content']"));
+            if(pnlTip.Count==1)
+            {
+                IWebElement btnCross = driver.FindElement(By.XPath("//div[@class='components-popover__content']/button[contains(@class,'nux-dot-tip__disable')]"));
+                btnCross.Click();
+            }
+
+            IWebElement txtTitle = driver.FindElement(By.XPath("//div[@class='editor-writing-flow block-editor-writing-flow']//div[@class='editor-post-title']//textarea[@placeholder='Add title']"));
+            txtTitle.SendKeys("Blog Post 1");
+            txtTitle.SendKeys(Keys.Tab);
+
+            IWebElement txtPara = driver.FindElement(By.XPath("//div[@class='editor-writing-flow block-editor-writing-flow']//div[@class='editor-block-list__block-edit block-editor-block-list__block-edit']//div/p[@role='textbox']"));
+            txtPara.SendKeys("This is the first line.\n");
+            txtPara.SendKeys("This is the second line.\n");
+            txtPara.SendKeys("This is the last line.\n");
+
+            IWebElement btnPublish = driver.FindElement(By.XPath("//div[@class='edit-post-header']/div[@class='edit-post-header__settings']/button[contains(text(),'Publish')]"));
+            btnPublish.Click();
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div[@class='edit-post-header']/div[@class='edit-post-header__settings']/button[contains(text(),'Publish')]")));
+
+            btnPublish.SendKeys(Keys.Return);
+
         }
 
         [TestCleanup]
         public void CleanUp()
         {
+            Actions action = new Actions(driver);
+            action.MoveToElement(driver.FindElement(By.XPath("//ul[@class='ab-top-secondary ab-top-menu']/li/a"))).Perform();
+
             IWebElement lnkLogOut = driver.FindElement(By.XPath("//ul[@class='ab-submenu']/li[@id='wp-admin-bar-logout']/a[text()='Log Out']"));
             lnkLogOut.Click();
 
--- SeleniumDemo/README.md
+++
@@ -1,3 +0,0 @@
-﻿This project is an attempt to create a Test Automation Solution using Selenium with C# from scratch.
-
-1) Start Off with automating a flow of a web application using a basic framework.
